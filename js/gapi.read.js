574fa18583cbb9e75cbdadd57737cf6d02ac0f0c
Georg Glas <georg@glas.eu.org>
2020-01-23 22:50:33 +0100

    make sure we are updating the image if we retrive a matching memberId

--- js/gapi.read.js
+++ js/gapi.read.js
@@ -10,14 +10,15 @@ function querySheet() {
     request.then(function(response) {
                 // Handle the results here (response.result has the parsed body).
                 console.log(response.result);
-                populateSheet(response.result);
+                updateInputForms(response.result);
+                updateImages(response.result);
               },
               function(reason) {
                   console.error('error: ' + reason.result.error.message);
               });
 }
 // populate html code
-function populateSheet(result) {
+function updateInputForms(result) {
   // i, j query matrix 
   // g output row matrix
   var i=0, j=0; g=0; 
@@ -37,5 +38,19 @@ function populateSheet(result) {
           }
          }  
       }
+  }
+}
+
+// populate html code
+function updateImages(result) {
+     // check if we have a memberId thats matching
+  var i=0, j=0; g=0; 
+  for(var range = 0; range < result.valueRanges.length; range++) {
+      for(var row=0; row<result.valueRanges[range].values.length; row++, i++) {
+        if (queryString("memberId") != null && result.valueRanges[range].values[row][1]==queryString("memberId")) {
+			g++
+			document.getElementById(g+":"+j).src = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl="+encodeURIComponent(result.valueRanges[range].values[row][col]);
+        }
+      }  
   }
 }
