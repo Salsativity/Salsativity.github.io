ba4835e149316182a18441ba9df895e2656ab95e
Georg Glas <georg@glas.eu.org>
2020-01-23 21:23:47 +0100

    added support for Edit URL

--- js/gapi.read.js
+++ js/gapi.read.js
@@ -2,7 +2,7 @@
 function querySheet() {
      var params = {
      "spreadsheetId": DestinationGoogleSpreadsheetId,
-     "ranges": [ DestinationGoogleSheetRange ],
+     "ranges": DestinationGoogleSheetRange,
      "valueRenderOption": "FORMATTED_VALUE",
      "dateTimeRenderOption": 'SERIAL_NUMBER'
     };
