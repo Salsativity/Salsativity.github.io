675b9714e97a4b699062fac531c26fdb3e49b619
Georg Glas <georg@glas.eu.org>
2020-01-24 11:41:01 +0100

    change ID of QR IMG Field and QR A Field

--- js/gapi.read.js
+++ js/gapi.read.js
@@ -11,7 +11,7 @@ function queryPrivateSheet() {
                 // Handle the results here (response.result has the parsed body).
                 console.log(response.result);
                 updateInputForms(response.result);
-                updateImages(response.result);
+                updateImagesPrivate(response.result);
               },
               function(reason) {
                   console.error('error: ' + reason.result.error.message);
@@ -46,7 +46,7 @@ function updateInputForms(result) {
       // loop through all the google sheet rows within the range
       for(var row=0; row<result.valueRanges[range].values.length; row++, i++) {
           // check if the memberId matches the first field
-          if (queryString("memberId") != null && result.valueRanges[range].values[row][1]==queryString("memberId")) {
+          if (queryString("memberId") != null && result.valueRanges[range].values[row][0]==queryString("memberId")) {
             g++
          }
          // loop through all the google sheet columns in this row and range
@@ -56,7 +56,7 @@ function updateInputForms(result) {
                  document.getElementById(0+":"+j).value = result.valueRanges[range].values[row][col];
              }
              // write values if queryString Memberid matches the first field in the row
-             if (queryString("memberId") != null && result.valueRanges[range].values[row][1]==queryString("memberId"))  {
+             if (queryString("memberId") != null && result.valueRanges[range].values[row][0]==queryString("memberId"))  {
                  document.getElementById(g+":"+j).value = result.valueRanges[range].values[row][col];
           }
          }  
@@ -65,17 +65,20 @@ function updateInputForms(result) {
 }
 
 // populate qr image
-function updateImages(result) {
-     // check if we have a memberId thats matching
+function updateImagesPrivate(result) {
+  // i, j query matrix 
+  // g output row matrix (future improvement: more output rows)
   var i=0, j=0; g=0; 
+  // loop through all the ranges (future improvement: request multiple ranges so we don´t need to load the full spreadsheet/full range) 
   for(var range = 0; range < result.valueRanges.length; range++) {
+      // loop through all the google sheet rows within the range
       for(var row=0; row<result.valueRanges[range].values.length; row++, i++) {
-        // check if queryString Memberid matches the first field in the row
+        // check if queryString Memberid matches the second field [1] in the row
         if (queryString("memberId") != null && result.valueRanges[range].values[row][1]==queryString("memberId")) {
             g++
-            // check if queryString Memberid matches the first field in the row and update grafic link
-            document.getElementById(g+":6-img").src = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl="+encodeURIComponent(result.valueRanges[range].values[g][4]);
-            document.getElementById(g+":6-a").href = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl="+encodeURIComponent(result.valueRanges[range].values[g][4]);
+            // check if queryString Memberid matches the second [1] field and update QR Code and link
+            document.getElementById(g+":QR-img").src = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl="+encodeURIComponent(result.valueRanges[range].values[g][4]);
+            document.getElementById(g+":QR-a").href = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl="+encodeURIComponent(result.valueRanges[range].values[g][4]);
         }
       }  
   }
