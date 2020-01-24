c2891c27fa28d6cf957dbf745b109fdbf11d2210
Georg Glas <georg@glas.eu.org>
2020-01-24 12:51:01 +0100

    updateQRCode fixes

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,6 @@
 
 function updateQRCode(row,href){
 	document.getElementById(row+":QR-a").href = href
-	document.getElementById(row+"QR-img").src = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl="+encodeURIComponent(urlencodhref)
+	document.getElementById(row+"QR-img").src = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl="+encodeURIComponent(href)
 
 }
