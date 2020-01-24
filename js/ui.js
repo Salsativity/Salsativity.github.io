83ecfb6f2b0a3181c9e58c8afed2e982aa9896dc
Georg Glas <georg@glas.eu.org>
2020-01-24 16:40:31 +0100

    added loadMemberId function

--- js/ui.js
+++ js/ui.js
@@ -4,3 +4,7 @@ function updateQRCode(row,href){
 	document.getElementById(row+":QR-img").src = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl="+encodeURIComponent(href);
 
 }
+
+function loadMemberId(id) {
+	window.location.search = "?memberId=" + id;
+}
