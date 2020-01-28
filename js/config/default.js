16c57013ae105a06f92f80c4fe5299231cc02c6f
georg-glas-salsativity-org <60177882+georg-glas-salsativity-org@users.noreply.github.com>
2020-01-28 13:58:23 +0100

    configuration
    we should be able to load configuration settings by URL Parameter or Cookie Settings

--- js/config/default.js
+++ js/config/default.js
@@ -1,14 +1,20 @@
-var DestinationGoogleFormId = "1FAIpQLSdRMaP6VyoXWRKy4soR_RtRLL4DibFikwAYQFhb1lh2_GKx7A" // google sheet id from URL https://docs.google.com/forms/d/[...]/edit
-var DestinationGoogleFormEntryId = "191679324" // thats the fieldId for the memberId //press the three vertical dashes in google forms and click on "link f�r Formalr mit Beispielwerten abrufen"
-var DestinationGooglePrivateSpreadsheetId = "17-hjfI9oEAsSmAEZcIkKfoMzpaTGIuJd3wVV5X6zMYY" // google spreadsheets id from URL https://docs.google.com/spreadsheets/d/[....]/edit
-var DestinationGooglePrivateSheetRange = [ "Formularantworten 1!B:G" ]
-var DestinationGooglePublicSpreadsheetId = "1QqKgOTHMOSraR3KsF6eipUMU2KUeGG_HarmyRy8GuTE" // google spreadsheets id from URL https://docs.google.com/spreadsheets/d/[....]/edit that has the importrange setup to only import public data
-var DestinationGooglePublicSheetRange = [ "Tabellenblatt1!A:G" ]
-var ContactDomain = "salsativity.org"
-var ContactAddress = "anmeldungen"
-var GoogleApiKey = 'AIzaSyBlGosN-bpWSGekqiZ0GzlLxxsZcW0zlMg';  // this key is restricted to referer salsativity.github.io
-var GoogleClientId = '43003984043-u7tnn8lve8psj8ogpv6h8vurm4pftah8.apps.googleusercontent.com';  // this ClientId is restricted to internal salsativity users only
-      // TODO: Authorize using one of the following scopes:
-      //   'https://www.googleapis.com/auth/spreadsheets'
-      //   'https://www.googleapis.com/auth/spreadsheets.readonly'
-var GoogleApiPermissionScope = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+// these are the sane defaults
+var DestinationGoogleFormId =
+  "1FAIpQLSdRMaP6VyoXWRKy4soR_RtRLL4DibFikwAYQFhb1lh2_GKx7A"; // google sheet id from URL https://docs.google.com/forms/d/[...]/edit
+var DestinationGoogleFormEntryId = "191679324"; // thats the fieldId for the memberId //press the three vertical dashes in google forms and click on "link f�r Formalr mit Beispielwerten abrufen"
+var DestinationGooglePrivateSpreadsheetId =
+  "17-hjfI9oEAsSmAEZcIkKfoMzpaTGIuJd3wVV5X6zMYY"; // google spreadsheets id from URL https://docs.google.com/spreadsheets/d/[....]/edit
+var DestinationGooglePrivateSheetRange = ["Formularantworten 1!B:G"];
+var DestinationGooglePublicSpreadsheetId =
+  "1QqKgOTHMOSraR3KsF6eipUMU2KUeGG_HarmyRy8GuTE"; // google spreadsheets id from URL https://docs.google.com/spreadsheets/d/[....]/edit that has the importrange setup to only import public data
+var DestinationGooglePublicSheetRange = ["Tabellenblatt1!A:G"];
+var ContactDomain = "salsativity.org";
+var ContactAddress = "anmeldungen";
+var GoogleApiKey = "AIzaSyBlGosN-bpWSGekqiZ0GzlLxxsZcW0zlMg"; // this key is restricted to referer salsativity.github.io
+var GoogleClientId =
+  "43003984043-u7tnn8lve8psj8ogpv6h8vurm4pftah8.apps.googleusercontent.com"; // this ClientId is restricted to internal salsativity users only
+// TODO: Authorize using one of the following scopes:
+//   'https://www.googleapis.com/auth/spreadsheets'
+//   'https://www.googleapis.com/auth/spreadsheets.readonly'
+var GoogleApiPermissionScope =
+  "https://www.googleapis.com/auth/spreadsheets.readonly";
--- js/cookies.js
+++ js/cookies.js
@@ -1,21 +1,21 @@
-function alertroles() {
-  var roles = getCookie('roles');
-  alert ("roles:"+roles);
+function alertCookie(cname) {
+  var cookieContent = getCookie("cname");
+  alert(cname + ":" + cookieContent);
 }
 function setCookie(cname, cvalue, exdays) {
   var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  var expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
   var name = cname + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
     var c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
